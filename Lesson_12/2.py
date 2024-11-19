7491a9c921c423a2a136df60d0b8ae80a4fe8fdc
Zaru14622 <pesseryj944@gmail.com>
2024-11-19 11:01:44 +0300

    19.11.2024

--- Lesson_12/2.py
+++ Lesson_12/2.py
@@ -1,12 +1,20 @@
 #2 списк 10на10(генератор) вывесте все элементы которые есть в 1-ом списке и нет во 2-ом
 from random import randint
-a =[randint(0, 100) for j in range(0, 10)]
-b =[randint(0, 100) for i in range(0, 10)]
-a = set(a)
-b = set(b)
-print(a)
-print(b)
+a =[[randint(0, 100) for i in range(0, 10)] for j in range(0, 10)]
+b =[[randint(0, 100) for i in range(0, 10)] for j in range(0, 10)]
+m1 = []
+m2 = []
+for i in a:
+    for j in i:
+        m1.append(j)
+for i in b:
+    for j in i:
+        m2.append(j)
+m1 = set(m1)
+m2 = set(m2)
+print(m1)
+print(m2)
 print("*"*40)
-print(a-b)
+print(m1 - m2)
 
 
